54639b8685cbcfa22a3baf27f3f7ea51e847949d
YoungJin Yun <30815shine@naver.com>
2025-09-18 16:25:13 +0900

    live camera filter implementation

--- src/filters.py
+++ src/filters.py
@@ -3,26 +3,36 @@ import numpy as np
 
 
 class Filters:
-    # TODO: Image kernels
     Kernels = {
+        'Original': np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]]),
+        'Blur': (1/9) * np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], dtype=np.float32),
+        'Gaussian blur':(1 / 16) * np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]], dtype=np.float32),
+        'Sharpen': np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]]),
+        'Sobel (x)': np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]),
+        'Sobel (y)': np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]]),
+        'Edge Detection': np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]]),
+        'Emboss': np.array([[-2, -1, 0], [-1, 1, 1], [0, 1, 2]])
     }
 
     def __init__(self, kernels=Kernels):
         self.kernels = kernels
-        # TODO: Implement internal variables
+        self.filter_names = list(self.kernels.keys())  # 필터 이름 리스트
+        self.current_index = 0  # 현재 필터 인덱스
+        self.current_filter = self.filter_names[self.current_index]
 
-    def apply_filter(self, frame, filter_name) -> np.array:
-        # TODO: Apply the selected filter kernel to the frame
-        pass
+    def apply_filter(self, frame, filter_name=None) -> np.array:
+        if filter_name is None:
+            filter_name = self.current_filter
+        kernel = self.kernels[filter_name]
+        return cv2.filter2D(frame, -1, kernel)
 
     def get_current_filter_name(self) -> str:
-        # TODO: Return currently set kernels's name
-        pass
+        return self.current_filter
 
     def switch_next_filter(self):
-        # TODO: Update currently selected kernel to the next
-        pass
+        self.current_index = (self.current_index + 1) % len(self.filter_names)
+        self.current_filter = self.filter_names[self.current_index]
 
     def switch_previous_filter(self):
-        # TODO: Update currently selected kernel to the previous
-        pass
+        self.current_index = (self.current_index - 1) % len(self.filter_names)
+        self.current_filter = self.filter_names[self.current_index]
